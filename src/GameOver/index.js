26b3ca12e08bcb51da9e294eb433c21c728d631d
manuumoran <manu.moran0710@gmail.com>
2024-07-24 00:06:31 -0300

    Avanzado el tema de los puntajes

--- src/GameOver/index.js
+++ src/GameOver/index.js
@@ -4,16 +4,47 @@ import './GameOver.css'
 
 function GameOver() {
     const {
-        players
+        players,
+        playerName
     } = React.useContext(QuizContext)
 
-    return (
-        <div className='gameover-message-container'>
-            {players.map((player) => (
-                <h1 key={player.name} className='gameover-message'>{player.name} | {player.score}</h1>
-            ))}
-        </div>
-    ); 
+    let elements = []
+    const currentPlayerIndex = players.findIndex(player => player.name === playerName)
+
+    const renderItems = () => {
+        console.log(players[0]);
+        for (let i = 0; i < 10; i++) {
+            elements.push(<h1 key={players[i].name} className='gameover-message'>{players[i].name} | {players[i].score}</h1>)
+        }
+        
+        return elements
+    }
+
+    if (players.length > 10 && currentPlayerIndex > 10) {
+        console.log(elements);
+        return (
+            <div className='gameover-message-container'>
+                {renderItems()}
+                <h1 key={players[currentPlayerIndex].name} className='gameover-message'>{players[currentPlayerIndex].name} | {players[currentPlayerIndex].score}</h1>
+            </div>
+        ); 
+    } else  if (players.length > 10) {
+        for (let i = 0; i < 10; i++) {
+            return (
+                <div className='gameover-message-container'>
+                    {renderItems()}
+                </div>
+            );         
+        }
+    } else {
+        return (
+            <div className='gameover-message-container'>
+                {players.map((player) => (
+                    <h1 key={player.name} className='gameover-message'>{player.name} | {player.score}</h1>
+                ))}
+            </div>
+        ); 
+    }
 }
 
 export { GameOver };
