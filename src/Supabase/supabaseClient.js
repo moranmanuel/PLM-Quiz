01dae7781ac00acf94ad3e18cd7ab24e299a0e68
manuumoran <manu.moran0710@gmail.com>
2024-08-12 17:03:38 -0300

    commit

--- src/Supabase/supabaseClient.js
+++ src/Supabase/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+console.log(supabaseUrl);
+console.log(supabaseKey);
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
